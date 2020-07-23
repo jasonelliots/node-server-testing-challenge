38579477bfb3bd8f5dbc6ed8dc0b50fcb8691180
jasonelliots <j.elliotschwartz@gmail.com>
2020-07-23 13:59:14 -0700

    completed 2 tests

---
+++ api/server.test.js
@@ -0,0 +1,40 @@
+const supertest = require("supertest");
+
+const server = require("./server.js");
+
+const Users = require("../users/usersModel");
+const db = require("../data/dbConfig.js");
+
+
+describe("create", function() {
+
+    beforeEach(async () => {
+        await db("users").truncate();
+    });
+
+    it("can create a new user", async () => {
+       await Users.insert({ username: "tester"});
+
+       const users = await db('users');
+
+       expect(users).toHaveLength(1)
+    })
+
+    it("should response with json", function () {
+        return supertest(server)
+            .get("/users")
+            .then(res => {
+                expect(res.type).toMatch(/json/i);
+            });
+    })
+
+})
+
+
+
+// describe("delete", function() {
+
+
+
+    
+// })
